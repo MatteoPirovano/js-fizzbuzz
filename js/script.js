987825be4f57f858e1e92451dc6465d8ca20f553
Matteo Pirovano <Matteo.Pirovano.Job@gmail.com>
2020-11-17 17:43:08 +0100

    fine

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@ for (var i = 1;  i  <  101;  i++) {
 
   if (i % 15 == 0) {
     console.log('FizzBuzz');
-    document.getElementById('numeri').innerHTML += '<li>' + 'FizzBuzz' + '</li>';
+    document.getElementById('numeri').innerHTML += '<li id="fb">' + 'FizzBuzz' + '</li>';
   } else if (i % 3 == 0) {
    console.log('Fizz');
-   document.getElementById('numeri').innerHTML += '<li>' + 'Fizz'+'</li>';
+   document.getElementById('numeri').innerHTML += '<li id="f">' + 'Fizz'+'</li>';
  }  else if (i % 5 == 0) {
    console.log('Buzz');
-   document.getElementById('numeri').innerHTML += '<li>' + 'Buzz' + '</li>';
+   document.getElementById('numeri').innerHTML += '<li id="b">' + 'Buzz' + '</li>';
  } else {
    console.log(i);
    document.getElementById('numeri').innerHTML +='<li>'+ i + '</li>';
