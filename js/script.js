4a2318bb3ca99625e6c49976959ec3446bc398c2
Matteo Pirovano <Matteo.Pirovano.Job@gmail.com>
2020-11-17 16:23:04 +0100

    secondo commit

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 for (var i = 1;  i  <  101;  i++) {
   if (i % 15 == 0) {
     console.log("FizzBuzz");
+    document.getElementById('numeri').innerHTML = "FizzBuzz";
   } else if (i % 3 == 0) {
    console.log("Fizz");
+   document.getElementById('numeri').innerHTML = "Fizz";
  }  else if (i % 5 == 0) {
    console.log("Buzz");
+   document.getElementById('numeri').innerHTML = "Buzz";
  } else {
    console.log(i);
+   document.getElementById('numeri').innerHTML = i;
  }
  }
